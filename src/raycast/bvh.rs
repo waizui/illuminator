4499d68145ea84ab06b4154ee2dac5529af1cc39
waizui@Pi5 <lch01234@gmail.com>
2025-07-15 17:17:12 +0900

    build sah

--- src/raycast/bvh.rs
+++ src/raycast/bvh.rs
@@ -74,6 +74,7 @@ impl MortonCode for MortonPrim {
 struct Treelet {
     start_index: usize,
     nprimitives: usize,
+    root: Option<Arc<BVHBuildNode>>,
     nodes: Vec<Arc<BVHBuildNode>>, // root node of treelet
 }
 
@@ -120,32 +121,36 @@ impl BVH {
         // create LBVH treelets at bottom of BVH
         // find intervals of primitives for each treelet
         let mut treelets: Vec<Treelet> = Vec::new();
-        let mut start = 0;
-        let mut end = 1;
-        let prims_size = morton_prims.len();
-        while end <= prims_size {
-            // use hight 12 bits to cluster treelets
-            let mask = 0b00111111111111000000000000000000;
-            if (end == prims_size)
-                || (morton_prims[start].morton_code & mask)
-                    != (morton_prims[end].morton_code & mask)
-            {
-                let nprimitives = end - start;
-                let max_nodes = 2 * nprimitives - 1;
-                let mut nodes = Vec::with_capacity(max_nodes);
-                for _ in 0..max_nodes {
-                    nodes.push(Arc::new(BVHBuildNode::default()));
-                }
-                treelets.push(Treelet {
-                    start_index: start,
-                    nprimitives,
-                    nodes,
-                });
-                start = end;
-            }
 
-            end += 1;
-        }
+        {
+            let mut start = 0;
+            let mut end = 1;
+            let prims_size = morton_prims.len();
+            while end <= prims_size {
+                // use hight 12 bits to cluster treelets
+                let mask = 0b00111111111111000000000000000000;
+                if (end == prims_size)
+                    || (morton_prims[start].morton_code & mask)
+                        != (morton_prims[end].morton_code & mask)
+                {
+                    let nprimitives = end - start;
+                    let max_nodes = 2 * nprimitives - 1;
+                    let mut nodes = Vec::with_capacity(max_nodes);
+                    for _ in 0..max_nodes {
+                        nodes.push(Arc::new(BVHBuildNode::default()));
+                    }
+                    treelets.push(Treelet {
+                        start_index: start,
+                        nprimitives,
+                        root: None,
+                        nodes,
+                    });
+                    start = end;
+                }
+
+                end += 1;
+            }
+        };
 
         let mut p: Vec<Box<dyn Primitive>> = Vec::with_capacity(self.primitives.len());
         let ordered_prims = {
@@ -161,8 +166,8 @@ impl BVH {
             // i-th treelet
             // 30 bits morton code , 12 bits used for building treelet clusters
             let first_bit_index: i32 = 29 - 12;
-            let ordered_prims = Arc::clone(&ordered_prims);
-            let ordered_prims_offset = Arc::clone(&ordered_prims_offset);
+            let ordered_prims = ordered_prims.clone();
+            let ordered_prims_offset = ordered_prims_offset.clone();
             let mut nodes_created = 0;
             let root = self.emit_lbvh(
                 &mut tr.nodes[0..],
@@ -174,13 +179,21 @@ impl BVH {
                 first_bit_index,
             );
 
-            let total_nodes = Arc::clone(&total_nodes);
+            tr.root = Some(root);
+            let total_nodes = total_nodes.clone();
             let mut guard = total_nodes.lock().unwrap();
             *guard += nodes_created;
         });
 
-        //TODO: build upper
+        let mut treelet_roots: Vec<Arc<BVHBuildNode>> = Vec::with_capacity(treelets.len());
+        treelets.iter().for_each(|tr| {
+            if let Some(root) = &tr.root {
+                treelet_roots.push(root.clone());
+            }
+        });
+        self.build_sah(&treelet_roots, 0, treelets.len(), &total_nodes);
 
+        // swap ordered primitives and original primitives
         self.primitives = Arc::try_unwrap(ordered_prims).unwrap();
     }
 
@@ -198,7 +211,7 @@ impl BVH {
         if bit_index == -1 || nprimitives < self.node_prims_limit {
             let first_prim_offset = ordered_prims_offset.fetch_add(nprimitives, SeqCst);
             *created_nodes += 1;
-            let node = Arc::clone(&build_nodes[0]);
+            let node = build_nodes[0].clone();
             let mut bounds = Bounds3f::zero();
             unsafe {
                 let base_ptr = ordered_prims.as_ptr() as *mut Box<dyn Primitive>;
@@ -206,7 +219,7 @@ impl BVH {
                     let org_prim_index = morton_prim.prim_index;
                     let prim_box = self.primitives[org_prim_index].clone_as_box();
                     bounds = bounds.union(prim_box.bounds());
-                    // thread safe since first_prim_offset is unique per thread (from fetch_add)
+                    // it is thread safe since first_prim_offset is unique per thread
                     let cur_prim_index = first_prim_offset + i;
                     std::ptr::write(base_ptr.add(cur_prim_index), prim_box);
                 }
@@ -258,7 +271,7 @@ impl BVH {
                 bit_index - 1,
             );
 
-            let node = Arc::clone(&build_nodes[0]);
+            let node = build_nodes[0].clone();
             let axis = (bit_index % 3) as usize;
             unsafe {
                 let node_ptr = Arc::as_ptr(&node) as *mut BVHBuildNode;
@@ -266,6 +279,28 @@ impl BVH {
             }
             node
         }
+    }
+
+    /// build treelets node use Surface Area Heuristic
+    fn build_sah(
+        &mut self,
+        treelet_roots: &[Arc<BVHBuildNode>],
+        start: usize,
+        end: usize,
+        total_nodes: &Arc<Mutex<usize>>,
+    ) -> Arc<BVHBuildNode> {
+        let nnodes = end - start;
+        if nnodes == 1 {
+            return treelet_roots[start].clone();
+        }
+
+        let mut total_nodes = total_nodes.lock().unwrap();
+        (*total_nodes) += 1;
+
+        let mut node = Arc::new(BVHBuildNode::default());
+
+        //TODO: impl
+        node
     }
 }
 
