c6f37dbcaaec6a0f096f36d9b7df03726e72f48a
waizui@Pi5 <lch01234@gmail.com>
2025-07-25 10:16:26 +0900

    fix rename

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
-pub use crate::core::{tensor::Vec3f, tensor::Vec3x3f};
+pub use crate::core::{tensor::Vec3f, tensor::Mat3x3f};
 pub use crate::img::*;
 pub use crate::raycast::{Ray, Raycast, bvh::BVH, sphere::Sphere};
 
