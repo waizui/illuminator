7b8954f4adac0082df8d7fc85aaa57b3fb2b231f
waizui@Pi5 <lch01234@gmail.com>
2025-07-27 20:30:43 +0900

    chore

--- src/splat/render.rs
+++ src/splat/render.rs
@@ -1,6 +1,6 @@
 use crate::{
-    splat::{gaussian::Gaussian, io::read_ply},
     prelude::BVH,
+    splat::{gaussian::Gaussian, io::read_ply},
 };
 use anyhow::Result;
 use rayon::prelude::*;
@@ -20,20 +20,19 @@ impl SplatsRenderer {
             bvh.push(splats[i]);
         });
 
-        bvh.build(129, true);
+        bvh.build(65, true);
 
         Ok(SplatsRenderer { bvh })
     }
-
 }
 
 #[test]
 fn test_splats_render() {
     use crate::img::*;
     use crate::{
-        splat::{gaussian::Gaussian, render::SplatsRenderer},
         prelude::Vec3f,
         raycast::Ray,
+        splat::{gaussian::Gaussian, render::SplatsRenderer},
     };
     use image::{Rgb, RgbImage};
     use rayon::prelude::*;
@@ -41,22 +40,23 @@ fn test_splats_render() {
     let path = "./target/obj_011.ply";
     let gs = SplatsRenderer::from_ply(path).unwrap();
 
-    // let (w, h) = (32, 32);
-    let (w, h) = (512, 512);
+    let (w, h) = (32, 32);
+    // let (w, h) = (512, 512);
     let mut img: Image<Rgb<u8>> = Image::new(w, h);
 
     img.data_mut()
         .par_iter_mut()
+        .with_min_len(w)
         .enumerate()
         .for_each(|(i, pix)| {
             let (iw, ih) = (i % w, i / w);
-            let (lw, ly) = (8., 8.);
+            let (lw, ly) = (6., 6.);
             let (x, y) = (
                 iw as f32 * lw / (w - 1) as f32,
                 (h - ih) as f32 * ly / (h - 1) as f32,
             );
 
-            let org = Vec3f::vec([x - lw / 2., y - ly / 2., 1025.]);
+            let org = Vec3f::vec([x - lw / 2., y - ly / 2.,1025.]);
             let dir = Vec3f::vec([0., 0., -1.]);
             let ray = Ray::new(org, dir);
 
